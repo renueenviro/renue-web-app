ff3460ec73aa0520a304eeada4e8fe1a984cd462
fitosegrera <fitosegrera@gmail.com>
2021-12-02 17:35:59 -0500

    added gitignore

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from "@sveltejs/adapter-netlify";
 
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: netlify()
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+    adapter: adapter(),
+  },
 };
 
 export default config;
